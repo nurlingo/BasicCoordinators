332e36012f658da04620715028cacc281e9a0538
Nursultan Askarbekuly <n.askarbekuly@akvelon.com>
2019-12-12 00:02:09 +0300

    Initial Commit

---
+++ BasicCoordinators/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasicCoordinators
+//
+//  Created by Nursultan on 12.12.2019.
+//  Copyright © 2019 Nursultan Askarbekuly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
